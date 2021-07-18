9a0a57557fbdb57fc620096d91a4d2e9a65d5c21
everin-create <69110551+everin-create@users.noreply.github.com>
2021-07-18 13:40:00 +0530

    Add files via upload

---
+++ bar.py
@@ -0,0 +1,8 @@
+import pandas as pd 
+import plotly.express as px
+
+df=pd.read_csv("data.csv")
+print(df)
+
+fig=px.bar(df,x='Country',y="InternetUsers",title="internet users per country",color="Country")
+fig.show()
---
+++ main.py
@@ -0,0 +1,4 @@
+import pandas as pd
+data=[12,13,45,65,4,34,7]
+df=pd.DataFrame(data)
+print(df)
